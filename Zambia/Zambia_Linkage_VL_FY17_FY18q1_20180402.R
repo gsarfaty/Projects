a05e300514dc0f5dc3fe8d81ce7de5325eee530e
gsarfaty <grumbolo@gmail.com>
2018-04-03 11:51:47 -0400

    Update Zambia_Linkage_VL_FY17_FY18q1_20180402.R

--- Zambia/Zambia_Linkage_VL_FY17_FY18q1_20180402.R
+++ Zambia/Zambia_Linkage_VL_FY17_FY18q1_20180402.R
@@ -25,8 +25,8 @@ names(linkage_data) <- tolower(names(linkage_data))
 linkage_data2 <-linkage_data %>%
   filter(indicator %in% c("HTS_TST_POS", "HTS_TST_NEG", "TX_NEW", "TX_PVLS"),
          standardizeddisaggregate %in% c("Modality/Age/Sex/Result", "Age/Sex/HIVStatus", "Age/Sex/Indication/HIVStatus")) %>%
-  select(psnu,psnuuid,facilityuid,facility,indicator,standardizeddisaggregate,numeratordenom,age,sex,fy2017q1,fy2017q2,fy2017q3,fy2017q4,fy2017apr,fy2018q1) %>% 
-  group_by(psnu,psnuuid,facilityuid,facility,indicator,numeratordenom,age,sex,add=TRUE) %>% 
+  select(snu1,snu1uid,psnu,psnuuid,facilityuid,facility,fundingagency,implementingmechanismname,indicator,standardizeddisaggregate,numeratordenom,age,sex,fy2017q1,fy2017q2,fy2017q3,fy2017q4,fy2017apr,fy2018q1) %>% 
+  group_by(snu1,snu1uid,psnu,psnuuid,facilityuid,facility,fundingagency,implementingmechanismname,indicator,numeratordenom,age,sex,add=TRUE) %>% 
   summarize_at(vars(starts_with("fy2")),funs(sum(.,na.rm=TRUE))) %>% 
   ungroup() %>% 
   unite(indicator_unite,indicator,numeratordenom,sep="_") %>% 
@@ -35,10 +35,10 @@ linkage_data2 <-linkage_data %>%
 
 
 #create HTS_TST as sum of post and neg disaggs
-linkage_data2$HTS_TST_N <-rowSums(linkage_data2[,8:9], na.rm=TRUE)
+linkage_data2$HTS_TST_N <-rowSums(linkage_data2[,12:13], na.rm=TRUE)
 
 #reorder columns
-linkage_data2<-linkage_data2[c(1,2,3,4,5,6,7,13,8,9,10,12,11)]
+linkage_data2<-linkage_data2[c(1,2,3,4,5,6,7,8,9,10,11,17,12,13,14,16,15)]
 
 #get total numerator for our variables of interest & read in values as double
 totalN <- read_tsv("ICPI_FactView_Site_IM_Zambia_20180215_v1_3.txt", 
@@ -54,8 +54,8 @@ names(totalN) <-tolower(names(totalN))
 totalN2 <- totalN %>% 
   filter(indicator %in% c("HTS_TST", "HTS_TST_POS", "HTS_TST_NEG", "TX_NEW", "TX_PVLS"),
          standardizeddisaggregate %in% c("Total Numerator", "Total Denominator")) %>% 
-  select(psnu,psnuuid,facilityuid,facility,indicator,standardizeddisaggregate,numeratordenom,fy2017q1,fy2017q2,fy2017q3,fy2017q4,fy2017apr,fy2018q1) %>% 
-  group_by(psnu,psnuuid,facilityuid,facility,indicator,standardizeddisaggregate,numeratordenom,add=TRUE) %>% 
+  select(snu1,snu1uid,psnu,psnuuid,facilityuid,facility,fundingagency,implementingmechanismname,indicator,standardizeddisaggregate,numeratordenom,fy2017q1,fy2017q2,fy2017q3,fy2017q4,fy2017apr,fy2018q1) %>% 
+  group_by(snu1,snu1uid,psnu,psnuuid,facilityuid,facility,fundingagency,implementingmechanismname,indicator,standardizeddisaggregate,numeratordenom,add=TRUE) %>% 
   summarize_at(vars(starts_with("fy2")),funs(sum(.,na.rm=TRUE))) %>% 
   ungroup() %>% 
   unite(indicator_unite,indicator,numeratordenom,sep="_") %>% 
@@ -69,7 +69,7 @@ totalN2$sex <-totalN2$standardizeddisaggregate
 
 totalN2 <- totalN2 %>% 
   select(-standardizeddisaggregate) %>% 
-  select(psnu,psnuuid,facilityuid,facility,age,sex,qtr,HTS_TST_N,HTS_TST_NEG_N,HTS_TST_POS_N,TX_NEW_N,TX_PVLS_N,TX_PVLS_D)
+  select(snu1,snu1uid,psnu,psnuuid,facilityuid,facility,fundingagency,implementingmechanismname,age,sex,qtr,HTS_TST_N,HTS_TST_NEG_N,HTS_TST_POS_N,TX_NEW_N,TX_PVLS_N,TX_PVLS_D)
 
   
 #combine disagg df with total numerator
